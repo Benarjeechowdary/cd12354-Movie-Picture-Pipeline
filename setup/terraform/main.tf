e52818ab84e066b037452a084cc027c283e8682a
Benarjeechowdary <115145711+Benarjeechowdary@users.noreply.github.com>
2026-09-08 08:03:22 +0530

    Add CI/CD pipelines and configure AWS EKS

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -197,7 +197,7 @@ resource "aws_eks_node_group" "main" {
   version         = aws_eks_cluster.main.version
   node_role_arn   = aws_iam_role.node_group.arn
   subnet_ids      = [var.enable_private == true ? aws_subnet.private_subnet.id : aws_subnet.public_subnet.id]
-  release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
+  ami_type        = "AL2023_x86_64_STANDARD"
   instance_types  = ["t3.small"]
 
   scaling_config {
@@ -309,22 +309,4 @@ resource "aws_iam_role_policy_attachment" "codebuild" {
   role       = aws_iam_role.codebuild.name
 }
 
-####################
-# Github Action role
-####################
-resource "aws_iam_user" "github_action_user" {
-  name = "github-action-user"
-}
 
-resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   = aws_iam_user.github_action_user.name
-  policy = data.aws_iam_policy_document.github_policy.json
-}
-
-data "aws_iam_policy_document" "github_policy" {
-  statement {
-    effect    = "Allow"
-    actions   = ["ecr:*", "eks:*", "ec2:*", "iam:GetUser"]
-    resources = ["*"]
-  }
-}
